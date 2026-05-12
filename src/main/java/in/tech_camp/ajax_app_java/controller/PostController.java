0c7878302a76689ddb1bd7467c6d5c3fd017a309
yuna0422 <yuna.hayata@smart-group.co.jp>
2026-05-12 09:58:39 +0900

    修正

--- src/main/java/in/tech_camp/ajax_app_java/controller/PostController.java
+++ src/main/java/in/tech_camp/ajax_app_java/controller/PostController.java
@@ -25,7 +25,6 @@ public class PostController {
     model.addAttribute("postForm", new PostForm());
     return "posts/index";
   }
-  aseih
 
   @PostMapping("/posts")
   public ResponseEntity<PostEntity> savePost(@ModelAttribute("postForm") PostForm form){
